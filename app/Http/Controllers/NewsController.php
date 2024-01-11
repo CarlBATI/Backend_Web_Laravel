b6b432aadcb7e9c6619b5c65aec95bb255b03ae3
carl-a-van-roosbroeck <carl.a.van.roosbroeck@protonmail.me>
2024-01-11 22:07:46 +0100

    Add NewsController with CRUD methods for NewsItems

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreNewsItemsRequest;
+use App\Http\Requests\UpdateNewsItemsRequest;
+use App\Models\NewsItems;
+
+class NewsItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreNewsItemsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(NewsItems $newsItems)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(NewsItems $newsItems)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateNewsItemsRequest $request, NewsItems $newsItems)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(NewsItems $newsItems)
+    {
+        //
+    }
+}
