46995ab0a0c35ffb8a2aab65848954e77d977c3f
carl-a-van-roosbroeck <carl.a.van.roosbroeck@protonmail.me>
2024-01-11 12:23:19 +0100

    Update AboutCategorySeeder and AboutLinkSeeder with first categories and links, updated Backlog.

--- database/seeders/AboutCategorySeeder.php
+++ database/seeders/AboutCategorySeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\AboutCategory;
 
 class AboutCategorySeeder extends Seeder
 {
@@ -12,6 +13,7 @@ class AboutCategorySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        AboutCategory::create(['id' => 1, 'name' => 'documentation']);
+        AboutCategory::create(['id' => 2, 'name' => 'code']);
     }
 }
--- database/seeders/AboutLinkSeeder.php
+++ database/seeders/AboutLinkSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\AboutLink;
 
 class AboutLinkSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class AboutLinkSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        AboutLink::create(['url' => 'https://laravel.com/docs/10.x', 'text' => 'Laravel 10.x Documentation', 'category_id' => 1]);
     }
 }
