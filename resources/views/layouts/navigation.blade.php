e181295f7e425ab8592941fbda18e3238d578104
carl-a-van-roosbroeck <carl.a.van.roosbroeck@protonmail.me>
2024-01-12 13:30:07 +0100

    Add authentication check to parts of navigation and settings dropdown in navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -29,6 +29,7 @@
             </div>
 
             <!-- Settings Dropdown -->
+            @auth
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
@@ -61,6 +62,7 @@
                     </x-slot>
                 </x-dropdown>
             </div>
+            @endauth
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
@@ -89,6 +91,7 @@
         </div>
 
         <!-- Responsive Settings Options -->
+        @auth
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
@@ -112,5 +115,6 @@
                 </form>
             </div>
         </div>
+        @endauth
     </div>
 </nav>
