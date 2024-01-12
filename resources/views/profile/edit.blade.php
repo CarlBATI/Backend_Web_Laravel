2c3273cdca731792aedd65d6321718996c331762
carl-a-van-roosbroeck <carl.a.van.roosbroeck@protonmail.me>
2024-01-12 23:07:42 +0100

    Refactor profile edit view layout to  allow full display of tokens

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout :showNavigation="true">
-    <div>{{ $user }}</div>
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
@@ -9,7 +8,7 @@
             </div>
 
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
+                <div class="max-w-xl md:max-w-2xl lg:max-w-4xl">
                     @include('profile.partials.personal-access-tokens-form')
                 </div>
             </div>
